27cbd550707251c5935d7473b5ab96af820c1d86
AnjelicaB <hmni.anjelica@gmail.com>
2023-01-09 12:57:10 -0800

    Added comment for graphics.cpp
    Added a short description for graphics.cpp to indicate its use for this project (a reference/startingpoint)

--- graphics.cpp
+++ graphics.cpp
@@ -1,3 +1,9 @@
+/**
+ * This file is for reference for the rest of the game
+ * 
+ * Imported from GitHub: https://gist.github.com/thales17/fb2e4cff60890a51d9dddd4c6e832ad2
+*/
+
 #include <iostream>
 #include <SDL2/SDL.h>
 
