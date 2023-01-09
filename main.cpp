4a7820666faf3af6d00d52030eca4ffb986addf6
AnjelicaB <hmni.anjelica@gmail.com>
2023-01-09 12:43:18 -0800

    Update main.cpp
    Added namespace std and rough docstring.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
-
 /**
- * this is a whats it called
+ * This is the main file of the game. It will call the controller file
+ * to run the game...
 */
+
+#include <iostream>
+using namespace std;
+
 int main(){
     std::cout << "hehehehehe" << endl;
 }
