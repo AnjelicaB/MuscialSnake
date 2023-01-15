f7e5d0d0a99071bbd72de08000b4d5f58059da30
AnjelicaB <hmni.anjelica@gmail.com>
2023-01-15 13:15:11 -0800

    Update models.cpp
    Added brainstorming and spec for some of the classes

--- gamefolder/models.cpp
+++ gamefolder/models.cpp
@@ -3,10 +3,70 @@
  * 
 */
 
+/**
+ * Returns the x-position of the 
+*/
+int positionX(Snake object){
+
+}
+
+int positionX(FoodItem object){
+    
+}
+
+/**
+ * This class will act as a helper to the Snake class. 
+ * It is a square/circle (TBD) of the same width/height as each square in the grid.
+ * 
+ * Attribute
+ * width
+ * height
+ * x
+ * y
+ * forward reference (SnakeUnit class)
+ * 
+ * Methods
+ * getter x
+ * getter y
+*/
+class SnakeUnit{
+
+};
+
+/**
+ * This class will use SnakeUnit as an attribute for each unit of the snake.
+ * The snake moves one unit per frame. When the snake moves, the last unit will
+ * be removed and one more unit will be added in front of the head depending on
+ * user input (up, down, right, left). When the snake grows in size, one more unit
+ * will be added in front of the head but the last unit will NOT be removed.
+ * 
+ * A reference to the head and the tail will be kept as an attribute of SnakeUnit class.
+ * The head will determine where to add another unit when the snake moves.
+ * The tail will have a forward reference attribute to determine
+ * where the next tail will be when the tail is removed.
+ * 
+ * Attributes: 
+ * length: number of snake unites
+ *  Initial length (in constant)
+ * head: snakeunit object
+ * tail: snakeunit object
+ * 
+ * methods:
+ * return length
+ * increment length
+*/
 class Snake{
 
 };
 
+/**
+ * Attribute
+ * x
+ * y
+ * Methods
+ * getter x
+ * getter y
+*/
 class FoodItem{
 
 };
@@ -15,6 +75,6 @@ class LengthApple : public FoodItem{
 
 };
 
-class SpeedApple :public FoodItem{
+class SpeedApple : public FoodItem{
 
 };
