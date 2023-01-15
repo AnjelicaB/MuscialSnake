0bcd0c669082386630db69941c8ab527612ec3fc
AnjelicaB <hmni.anjelica@gmail.com>
2023-01-15 12:26:31 -0800

    Update models.cpp
    Added class headers.

--- gamefolder/models.cpp
+++ gamefolder/models.cpp
@@ -2,3 +2,19 @@
  * This file will contain all of the classes necessary for the game
  * 
 */
+
+class Snake{
+
+};
+
+class FoodItem{
+
+};
+
+class LengthApple : public FoodItem{
+
+};
+
+class SpeedApple :public FoodItem{
+
+};
