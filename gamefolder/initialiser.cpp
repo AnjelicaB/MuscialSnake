936bea771d2ded96073d11dc3c9be3b7b9d1d8d0
Em <emketterer07@gmail.com>
2023-02-01 19:13:44 +0100

    This should initialise SDL2 and IMG (for loading images later on)

--- gamefolder/initialiser.cpp
+++ gamefolder/initialiser.cpp
@@ -4,3 +4,24 @@
  * 
  * Creators: Bian, Anjelica; Ketterer, Em
 */
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL_image.h>
+
+/**
+ * initialises SDL and IMG 
+ * returns 1 if either of the init calls fail
+*/
+
+int main(){
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        printf("SDL_Init failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    if(IMG_Init(IMG_INIT_PNG) < 0) {
+        printf("IMG_Init failed: %s\n", IMG_GetError());
+        return 1;
+    }
+}
